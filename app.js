b8c1dffd685295273b6365ad753502404337dcda
shivam pandey <shivampandey9540@gmail.com>
2022-11-29 16:16:48 +0530

    cors issue

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const app = express();
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
+const corsOptions = {
+  origin: "https://todofrontend-txbl.vercel.app/",
+  optionsSucccessStatus: 200,
+};
 const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", route);

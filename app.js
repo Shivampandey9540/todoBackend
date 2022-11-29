4095df432d4fa10afcaf27e678408958b9dea686
shivam pandey <shivampandey9540@gmail.com>
2022-11-29 16:43:47 +0530

    cors add

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
 const corsOptions = {
-  origin: "https://todoappshivampandey.herokuapp.com/",
+  origin: "https://todofrontend-txbl.vercel.app/",
   optionsSucccessStatus: 200,
 };
 const cors = require("cors");
